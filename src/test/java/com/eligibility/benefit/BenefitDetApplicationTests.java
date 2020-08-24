08fdf92fdeb03c66aa3f2e3f7559cd7d2c7ab1f1
Saiprasad Mahadevan <saiprasad.mahadevan@perficient.com>
2020-08-24 21:30:50 +0530

    adding target to eligibility-app

--- src/test/java/com/eligibility/benefit/BenefitDetApplicationTests.java
+++ src/test/java/com/eligibility/benefit/BenefitDetApplicationTests.java
@@ -1,13 +1,12 @@
 package com.eligibility.benefit;
 
-
-import org.junit.Test;
+//import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BenefitDetApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
