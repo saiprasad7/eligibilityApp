0bc86ca2f3091bf8da4f648572ac862142c9a29d
Saiprasad Mahadevan <saiprasad.mahadevan@perficient.com>
2020-08-28 15:25:07 +0530

    added the logs and change the condition in getBenefit

--- src/main/java/com/eligibility/benefit/Service/EligibilityService.java
+++ src/main/java/com/eligibility/benefit/Service/EligibilityService.java
@@ -1,5 +1,7 @@
 package com.eligibility.benefit.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,27 +13,44 @@ import com.eligibility.benefit.model.Subscribers;
 @Service
 public class EligibilityService {
 
+	protected Logger logger = LoggerFactory.getLogger(EligibilityService.class);
 	@Autowired
 	private SubscriberRepository subscriberRepository;
 
 	public EligibilityCheck getEligibility(String subscriberId, String uniqueId, String plan) {
+		logger.info("Getting Eligibility info");
 		EligibilityCheck eligible= new EligibilityCheck();
-		Boolean isEligibile=false;
+		//boolean isEligibile=false;
+		try {
 		Subscribers subscribers=subscriberRepository.findBySubscriberId(subscriberId);
-		if(subscribers.getDependents().get(0).getDependentId().equals(uniqueId)&&subscribers.getBenefits().get(0).getPolicyId().equals(plan)) {
-			eligible.setSubscriberId(subscribers.getSubscriberId());
-			eligible.setUniqueId(subscribers.getDependents().get(0).getDependentId());
-			eligible.setPlanCode(subscribers.getBenefits().get(0).getPolicyId());
-			if(subscribers.getDependents().get(0).getDependentBenefits().get(0).getCurrentEligibleAmount()>0) {
-				isEligibile=true;
-				eligible.setEligible(isEligibile);   
-			}
-			eligible.setRelationShip(subscribers.getDependents());
-			eligible.setTotalEligibleAmount(subscribers.getBenefits().get(0).getTotalEligibleAmount());
-			eligible.setCurrentEligibleAmount(subscribers.getBenefits().get(0).getCurrentEligibleAmount());
+		if(null!=subscribers) {
+	//	boolean isEligibile=false;
+			subscribers.getDependents().stream().forEach(dependent -> {
+				boolean isEligibile=false;
+				if(dependent.getDependentId().equals(uniqueId)&& dependent.getDependentBenefits().get(0).getCurrentEligibleAmount()>0){
+					isEligibile=true;
+					subscribers.getBenefits().stream().forEach(benefitPlan -> {
+						if(benefitPlan.getPolicyId().equals(plan)) {
+							eligible.setSubscriberId(subscribers.getSubscriberId());	
+							eligible.setEligible(true);
+							eligible.setUniqueId(dependent.getDependentId());
+							eligible.setPlanCode(benefitPlan.getPolicyId());
+							eligible.setRelationShip(subscribers.getDependents());
+							eligible.setTotalEligibleAmount(benefitPlan.getTotalEligibleAmount());
+							eligible.setCurrentEligibleAmount(benefitPlan.getCurrentEligibleAmount());
+						    }
+					      });		
+						}
+			});
+			logger.info("the subsciber is eligible for the benefit");	
+	}
+	//logger.info("the subsciber is not eligible for the benefit");
 		}
+		catch(Exception e) {
+			logger.info("the subsciber is not eligible for the benefit"+e);
+		}
+	
 		return eligible;
-
 	}
 
 }
--- src/main/java/com/eligibility/benefit/controller/EligibilityController.java
+++ src/main/java/com/eligibility/benefit/controller/EligibilityController.java
@@ -1,6 +1,9 @@
 package com.eligibility.benefit.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +13,24 @@ import com.eligibility.benefit.model.EligibilityCheck;
 
 @RestController
 public class EligibilityController {
-	
+	protected Logger logger = LoggerFactory.getLogger(EligibilityController.class);
+	public static final int status =100;
 	@Autowired
 	private EligibilityService eligibilityService;
 	
 	@GetMapping(path="/getBenefits",produces = "application/json")
-	public EligibilityCheck getBenefit(@RequestParam String subscriberId,String uniqueId,String plan ) {
-		return eligibilityService.getEligibility(subscriberId, uniqueId, plan);
+	public ResponseEntity<EligibilityCheck> getBenefit(@RequestParam String subscriberId,String uniqueId,String plan ) {
+		logger.info("calling getBenefits API");
+			EligibilityCheck eligibilityCheck=eligibilityService.getEligibility(subscriberId, uniqueId, plan);
+			if(null!=eligibilityCheck.getRelationShip()||true==eligibilityCheck.isEligible()) {
+				return   ResponseEntity.ok(eligibilityCheck);
+						}
+			else {
+				logger.info("null value-- not eligible");
+				return ResponseEntity.noContent().build();
+				
+			}	
+			
 	}
 	
 }
