bf37aea91a4a3f19ca253135e5db49c0a2960317
sonia.balamurugan <sonia.balamurugan@INDIA-Sonia.perficient.com>
2020-09-01 21:06:07 +0530

    Backend validation for emailid,password and policyid implementation commit

--- src/main/java/com/eligibility/benefit/Service/SubscriberService.java
+++ src/main/java/com/eligibility/benefit/Service/SubscriberService.java
@@ -2,6 +2,7 @@ package com.eligibility.benefit.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,37 @@ public class SubscriberService {
 		List <Dependents> dependentList=new ArrayList();
 		Benefit benefit=new Benefit();
 		Dependents dependents=new Dependents();
+		Random random = new Random();
+		int id = random.nextInt((999999999-100)+1)+10;
+		subscribers.setSubscriberId(String.valueOf(id));
+		try {
+		String email = subscribers.getEmail();
+		if (!isValidEmail(email)) {
+			throw new Exception("Invalid email id");
+		}
+		} catch (Exception e) {
+			 e.printStackTrace();
+			 return e.toString();
+		}
+		try {
+			String password = subscribers.getPassword();
+			if (!isValidPassword(password)) {
+				throw new Exception("Password must be at least 5 characters and at most 8 characters which contains atleast one number, upper case alphabet, lower case alphabet and special characters.");
+			}
+			} catch (Exception e) {
+				 e.printStackTrace();
+				 return e.toString();
+			}
+		try {
+			String policyId = subscribers.getBenefits().get(0).getPolicyId();
+			if ((!policyId.equals("0000000001")) && (!policyId.equals("0000000002"))
+					&& (!policyId.equals("0000000003")) && (!policyId.equals("0000000004"))) {
+				throw new Exception("Invalid Policy Code");
+			}
+			} catch (Exception e) {
+				 e.printStackTrace();
+				 return e.toString();
+			}
 		Policies policy=policiesService.getPolicyDetails((subscribers.getBenefits().get(0).getPolicyId()));
 		benefit.setId(policy.getId());
 		benefit.setPolicyId(policy.getPolicyId());
@@ -46,5 +78,19 @@ public class SubscriberService {
 		subscribers.setDependents(dependentList);
 		subscriberRepository.save(subscribers);
 		return "Inserted Successfully";
+		
 	}
+	
+	  public static boolean isValidEmail(String email) {
+	      String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+	      return email.matches(regex);
+	   }
+	  
+	  public static boolean isValidPassword(String password) {
+	      String regex = "^(?=.*[0-9])"
+                  + "(?=.*[a-z])(?=.*[A-Z])"
+                  + "(?=.*[@#$%^&+=])"
+                  + "(?=\\S+$).{5,8}$"; 
+	      return password.matches(regex);
+	   }
 }
