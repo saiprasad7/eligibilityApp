df0c50284be31bab23d2746e6b159ab682c04a4b
saiprasad.mahadevan <saiprasad.mahadevan@INDIA-SaiP.mshome.net>
2020-09-02 18:35:50 +0530

    eligibility check with empty dependent check

--- src/main/java/com/eligibility/benefit/util/Constants.java
+++ src/main/java/com/eligibility/benefit/util/Constants.java
@@ -4,7 +4,12 @@ public class Constants {
 	
 	public static final int PAGE_NOT_FND=404;
 	public static final String PAGE_NOT_FOUND="Page Not Found due to invalid Subscriber";
-	public static final int NO_CONT_CODE=203;
-	public static final String NO_CONTENT="No Content for that Dependent";
+	public static final int CONT_CODE=203;
+	public static final int NO_CONT_CODE=400;
+	public static final String NO_CONTENT="The given policy id/Subscriberid is invalid ";
+	public static final String INVALIDSUBS="The given Subscriberid is invalid ";
+	public static final String INVALIDPOLICY="The given policy id/Dependent Id is invalid ";
+	public static final String INVALIDDEPENDENT="The given Dependent Id is invalid ";
+	public static final int NULLCONT_CHECK=412;
 	
 }
