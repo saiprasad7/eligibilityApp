ff53b847f3cfe9c32e90946b99299c00779b2b4e
prabhakaran1662 <prabhu1662@gmail.com>
2020-09-23 21:31:54 +0530

    Temporary Password Java code

--- src/main/java/com/eligibility/benefit/Service/UserService.java
+++ src/main/java/com/eligibility/benefit/Service/UserService.java
@@ -17,6 +17,14 @@ import org.springframework.stereotype.Service;
 import com.eligibility.benefit.Repo.UserRepository;
 import com.eligibility.benefit.model.TokenDetails;
 import com.eligibility.benefit.model.Users;
+import com.eligibility.benefit.util.LoggerUtil;
+import com.eligibility.benefit.util.MailServices;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +34,22 @@ import lombok.extern.slf4j.Slf4j;
 public class UserService implements UserDetailsService {
 
 	protected Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private MailServices mailServices;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
 	public Users getByUsername(String name) {
 
 		Users dbUser = userRepository.findByName(name);
@@ -40,6 +58,66 @@ public class UserService implements UserDetailsService {
 
 	}
 
+	public JsonNode getTemporaryPassword(String tomail, String userName) {
+		LoggerUtil.infoLog(logger,"getTemporaryPassword method is started in service class");
+		JsonNode userNode = mapper.createObjectNode();
+		Query query = new Query();
+		try {
+			query.addCriteria(Criteria.where("name").is(userName).and("email").is(tomail));
+			Users exitUser = mongoTemplate.findOne(query, Users.class);
+			if (exitUser != null) {
+				((ObjectNode) userNode).put("subject", "Temporary Password");
+				((ObjectNode) userNode).put("message",
+						"Hi " + userName + "  \n\n Welcome to PERFICIENT online courses. Your Temporary Password is "
+								+ exitUser.getPassword() + " \n\n Please visit this link http://localhost:8080");
+				((ObjectNode) userNode).put("link", "Please visit this link http://localhost:8080");
+				((ObjectNode) userNode).put("to", tomail);
+				LoggerUtil.infoLog(logger,"getTemporaryPassword method is End in service class");
+				return userNode;
+			} else {
+				((ObjectNode) userNode).put("subject", "Temporary Password");
+				((ObjectNode) userNode).put("message", "Hi " + userName
+						+ "  \n\n Welcome to PERFICIENT online courses. You are not registered user. Please register online course. \n\n Please visit this link http://localhost:8080");
+				((ObjectNode) userNode).put("link", "Please visit this link http://localhost:8080");
+				((ObjectNode) userNode).put("to", tomail);
+				LoggerUtil.infoLog(logger,"getTemporaryPassword method is End in service class");
+				return userNode;
+			}
+		}catch(NullPointerException e) {
+			e.printStackTrace();
+			LoggerUtil.infoLog(logger,"getTemporaryPassword is error => "+e);
+		}catch(Exception e) {
+			LoggerUtil.infoLog(logger,"getTemporaryPassword is error => "+e);
+			e.printStackTrace();
+		}
+		return userNode;
+		
+	}
+
+	
+	public JsonNode sendTemporaryPassword(String toEmail, String userId) {
+		LoggerUtil.infoLog(logger,"sendTemporaryPassword is started");
+		JsonNode responceNode = mapper.createObjectNode();
+		try {
+			mailServices.sendMail(getTemporaryPassword(toEmail, userId));
+			((ObjectNode)responceNode).put("status", true);
+			((ObjectNode)responceNode).put("data",  "Email send successfully");
+			LoggerUtil.infoLog(logger,"sendTemporaryPassword is end");
+			return responceNode;
+		}catch(NullPointerException e) {
+			((ObjectNode)responceNode).put("status", false);
+			((ObjectNode)responceNode).put("data",  "Email Not send. Exception accured");
+			e.printStackTrace();
+			return responceNode;
+		}catch(Exception e) {
+			((ObjectNode)responceNode).put("status", false);
+			((ObjectNode)responceNode).put("data",  "Email Not send. Exception accured");
+			e.printStackTrace();
+			return responceNode;
+		}
+		
+	}
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		log.info("get the user name " + username);
--- src/main/java/com/eligibility/benefit/controller/UserController.java
+++ src/main/java/com/eligibility/benefit/controller/UserController.java
@@ -3,6 +3,7 @@ package com.eligibility.benefit.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eligibility.benefit.Service.UserService;
 import com.eligibility.benefit.model.TokenDetails;
+import com.eligibility.benefit.util.LoggerUtil;
 import com.eligibility.benefit.util.ResponseHandlingUtil;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +28,9 @@ public class UserController {
 	protected Logger logger = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private ObjectMapper mapper;
 	
 	@GetMapping(path="/getUser",produces = "application/json")
 	public ResponseEntity<Object> getUserDetails(@RequestParam String name) {
@@ -36,6 +44,27 @@ public class UserController {
 		log.info("calling API for token validation");
 		
 		return ResponseHandlingUtil.prepareResponse(userService.updateUserInfo(tokenDetails));
+	}
+	
+	@PostMapping(value = "/sendTemporaryPassword")
+	public ResponseEntity<JsonNode> sendTemporaryPassword(@RequestBody JsonNode userNode) {
+		LoggerUtil.infoLog(logger,"calling sendTemporaryPassword API");
+
+		JsonNode responceNode = mapper.createObjectNode();
+		try {
+			responceNode = userService.sendTemporaryPassword(userNode.get("email").asText(),
+					userNode.get("userName").asText());
+			LoggerUtil.infoLog(logger,"end sendTemporaryPassword API");
+			return new ResponseEntity<JsonNode>(responceNode, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			((ObjectNode)responceNode).put("status", false);
+			((ObjectNode)responceNode).put("data",  "Email Not send. Exception accured");
+			e.printStackTrace();
+			return new ResponseEntity<JsonNode>(responceNode, HttpStatus.NOT_FOUND);
+
+		}
+
 	}
 	
 }
