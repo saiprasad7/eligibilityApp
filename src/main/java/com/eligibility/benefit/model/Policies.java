e498f3ac4c776ff6e04f53ca121d8691f90f1ad4
Vibhor Kainth <vibhor.kainth@perficient.com>
2020-10-14 13:10:56 +0530

    Feature: Policies Service happy-path test

--- src/main/java/com/eligibility/benefit/model/Policies.java
+++ src/main/java/com/eligibility/benefit/model/Policies.java
@@ -1,24 +1,38 @@
 package com.eligibility.benefit.model;
 
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
+import java.util.Objects;
 
 @Data
 @Document
 public class Policies {
-	
-	@Id
-	private String id;
-	
-	private String policyId;
-	
-	private String policyName;
-	
-	private String policyBenefits;
-	
-	private Long claimableAmount;
 
+    @Id
+    private String id;
+
+    private String policyId;
+
+    private String policyName;
+
+    private String policyBenefits;
+
+    private Long claimableAmount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Policies policies = (Policies) o;
+        return Objects.equals(id, policies.id) &&
+                Objects.equals(policyId, policies.policyId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, policyId);
+    }
 }
--- src/test/java/com/eligibility/benefit/Service/PoliciesServiceTest.java
+++ src/test/java/com/eligibility/benefit/Service/PoliciesServiceTest.java
@@ -2,47 +2,64 @@ package com.eligibility.benefit.Service;
 
 import com.eligibility.benefit.Repo.PolicyRepository;
 import com.eligibility.benefit.model.Policies;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 public class PoliciesServiceTest {
 
+    @InjectMocks
     private PoliciesService policiesService;
 
     @Mock
     private PolicyRepository policyRepository;
 
-    @Mock
-    private Policies policy;
+    @Test
+    void whenPoliciesExistShouldRetrieveAllPolicyIds() {
+        Mockito.when(policyRepository.findAll()).thenReturn(policies());
 
-    @Mock
-    private List<Policies> policyList;
+        List<Policies> foundPolicies = policiesService.getAllPolicies();
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        policiesService = Mockito.mock(PoliciesService.class);
-    }
-
-    @AfterEach
-    public void tearDown() throws Exception {
-        policiesService = null;
+        Policies policy = policies().get(0);
+        Assertions.assertThat(foundPolicies.size()).isEqualTo(1);
+        Assertions.assertThat(foundPolicies.get(0)).isEqualTo(policy);
     }
 
     @Test
-    public void testGetPolicyDetails() {
-        Mockito.when(policiesService.getPolicyDetails("")).thenReturn(policy);
+    void whenPolicyIdsProvidedShouldRetrieveAllDetails() {
+        Mockito.when(policyRepository.findByPolicyId("123")).thenReturn(policies().get(0));
+        Policies policy = policies().get(0);
+
+        List<Policies> found = policiesService.getPolicyDetailsList(Collections.singletonList("123"));
+
+        Assertions.assertThat(found.size()).isEqualTo(1);
+        Assertions.assertThat(found.get(0)).isEqualTo(policy);
     }
 
-    @Test
-    public void testGetAllPolicies() {
-        Mockito.when(policiesService.getAllPolicies()).thenReturn(policyList);
+    private List<Policies> policies() {
+        List<Policies> policies = new ArrayList<>();
+        Policies policy = getPolicies();
+        policies.add(policy);
+        return policies;
+    }
+
+    private Policies getPolicies() {
+        Policies policy = new Policies();
+        policy.setPolicyName("name");
+        policy.setId("123");
+        policy.setPolicyId("123");
+        policy.setClaimableAmount(100L);
+        policy.setPolicyBenefits("benefits");
+        return policy;
     }
 
 }
